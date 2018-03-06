02a27d170d912b4f7e62e050a6d0d6fbbff8d6c4
Olecika Darii <olealea001@gmail.com>
2018-03-06 14:38:18 +0200

    problem: only two statistics

--- soccer/Game.java
+++ soccer/Game.java
@@ -1,7 +1,35 @@
 package soccer;
 
+import utility.GameUtils;
+
 public class Game {
    public Team homeTeam;
    public Team awayTeam;
    public Goal[] goals;
+
+   public void playGame(int max_goals) {
+      //Create an array containing a random number of goals (but <max) && use Math.random (why *, +1 ?!?!?!)
+      int nrGoals = (int) (Math.random() * max_goals + 1);
+      Goal[] goals = new Goal[nrGoals];
+      this.goals = goals;
+
+      //call GameUtil's addGameGoals() method && use proper import
+      GameUtils.addGameGoals(this);
+   }
+
+   //Override the playGame method to no arg method
+   public void playGame() {
+      playGame(6);
+   }
+
+   //Mathod that prints statistics for each goal scored
+   public String getStatistics() {
+      StringBuilder sb = new StringBuilder();
+      System.out.println("");
+      for (Goal goal : goals) {
+         System.out.println("The player " + goal.player.player_name + " of "
+                 + goal.team.team_name + " marked a goal on minute " + goal.time);
+      }
+      return sb.toString();
+   }
 }
--- soccer/League.java
+++ soccer/League.java
@@ -2,7 +2,21 @@ package soccer;
 
 public class League {
     public static void main(String[] args) {
-        //Instantiate and name six players
+        //Instantiate a League Object
+        League league = new League();
+
+        //Create games and teams through methods
+        Team[] teams = league.createTeams();
+        Game[] games = league.createGames(teams);
+
+        //Play a game and print the description (statistics)
+        Game game = games[0];
+        game.playGame();
+        System.out.println(game.getStatistics());
+    }
+
+    //Method to create two teams
+    public Team[] createTeams() {
         Player player1 = new Player();
         Player player2 = new Player();
         Player player3 = new Player();
@@ -28,67 +42,68 @@ public class League {
         team1.team_roster = new Player[]{player1, player2, player3};
         team2.team_roster = new Player[]{player4, player5, player6};
 
-        System.out.println("Members of "+team1.team_name+":");
-        for (Player player : team1.team_roster){
-            System.out.println(player.player_name);
-        }
-        System.out.println("");
-        System.out.println("Members of "+team2.team_name+":");
-        for (Player player : team2.team_roster){
-            System.out.println(player.player_name);
-        }
+        Team[] theTeams = {team1, team2};
+        return theTeams;
+    }
 
-        //Have the two teams play each other
+    //Method that creates a game
+    public Game[] createGames(Team[] teams) {
         Game game = new Game();
-        game.homeTeam = team1;
-        game.awayTeam = team2;
+        game.homeTeam = teams[0];
+        game.awayTeam = teams[1];
+        Game[] games = {game};
+        return games;
+    }
 
-        //Create three goals and assign them player, time and team
-        Goal goal1 = new Goal();
-        Goal goal2 = new Goal();
-        Goal goal3 = new Goal();
+//        System.out.println("Members of "+team1.team_name+":");
+//        for (Player player : team1.team_roster){
+//            System.out.println(player.player_name);
+//        }
+//        System.out.println("");
+//        System.out.println("Members of "+team2.team_name+":");
+//        for (Player player : team2.team_roster){
+//            System.out.println(player.player_name);
+//        }
 
-        goal1.player = game.homeTeam.team_roster[0];
-        goal1.team = game.homeTeam;
-        goal1.time = 10.00;
-
-        goal2.player = game.awayTeam.team_roster[0];
-        goal2.team = game.awayTeam;
-        goal2.time = 30.00;
-
-        goal3.player = game.awayTeam.team_roster[2];
-        goal3.team = game.awayTeam;
-        goal3.time = 45.00;
-
-        Goal goals[] = new Goal[]{goal1, goal2, goal3};
-        game.goals = goals;
-
-        //Print the time, players and teams
-        System.out.println("");
-        for (Goal goal : goals) {
-            System.out.println("The player "+goal.player.player_name+" of "
-                    +goal.team.team_name+" marked a goal on minute "+goal.time);
-        }
-
-        //search for the *Olg*
-        System.out.println("");
-        for (Player player : team2.team_roster) {
-            if (player.player_name.matches(".*Olg.")) {
-                System.out.println("Found " + player.player_name);
-            }
-        }
-
-        //display members of a team required way
-        for (Player player : team1.team_roster){
-            String first_name = player.player_name.substring(player.player_name.indexOf(" "));
-            String last_name = player.player_name.substring(0, player.player_name.indexOf(" "));
-            System.out.println(first_name+", "+last_name);
-        }
+//        //Create three goals and assign them player, time and team
+//        Goal goal1 = new Goal();
+//        Goal goal2 = new Goal();
+//        Goal goal3 = new Goal();
+//
+//        goal1.player = game.homeTeam.team_roster[0];
+//        goal1.team = game.homeTeam;
+//        goal1.time = 10.00;
+//
+//        goal2.player = game.awayTeam.team_roster[0];
+//        goal2.team = game.awayTeam;
+//        goal2.time = 30.00;
+//
+//        goal3.player = game.awayTeam.team_roster[2];
+//        goal3.team = game.awayTeam;
+//        goal3.time = 45.00;
+//
+//        Goal goals[] = new Goal[]{goal1, goal2, goal3};
+//        game.goals = goals;
+//
+//        //search for the *Olg*
+//        System.out.println("");
+//        for (Player player : team2.team_roster) {
+//            if (player.player_name.matches(".*Olg.")) {
+//                System.out.println("Found " + player.player_name);
+//            }
+//        }
+//
+//        //display members of a team required way
+//        for (Player player : team1.team_roster){
+//            String first_name = player.player_name.substring(player.player_name.indexOf(" "));
+//            String last_name = player.player_name.substring(0, player.player_name.indexOf(" "));
+//            System.out.println(first_name+", "+last_name);
+//        }
 
         //another method
         //Lastname, Firstname
 //        StringBuilder familyNameFirst = new StringBuilder();
-//        for (Player thePlayer: team1.playerArray) {
+//        for (Player thePlayer: team1.playerArray)
 //            String[] name = thePlayer.playerName.split(" ");
 //            familyNameFirst.append(name[1]);
 //            familyNameFirst.append(", ");
@@ -96,4 +111,4 @@ public class League {
 //            System.out.println(familyNameFirst);
 //            familyNameFirst.delete(0,familyNameFirst.length());
        }
-}
+
